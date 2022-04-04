ef10fb0388033aa2c253e1cd6f5589c643dbeca5
santhosh4456 <100783089+santhosh4456@users.noreply.github.com>
2022-04-04 13:24:51 +0530

    Create triple_assign3_map.py

---
+++ triple_assign3_map.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+list_of_numbers = [2,5,1,4,8,7]
+x = map(lambda x: x**2, list_of_numbers)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
